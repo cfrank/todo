86d8e3caf07130ca814f8b9a35610d55d0689b16
Chris Frank <chris@cfrank.org>
2018-12-27 05:47:20 -0500

    Add data managment
    Signed-off-by: Chris Frank <chris@cfrank.org>

---
+++ src/data.c
@@ -0,0 +1,62 @@
+// Copyright 2018 Chris Frank
+// Licensed under BSD-3-Clause
+// Refer to the license.txt file included in the root of the project
+
+#include <stdlib.h>
+
+#include "data.h"
+
+struct todo_data *create_todo_data(uint64_t id, uint64_t priority,
+                                   const struct state_data *state,
+                                   const char *subject, const char *description)
+{
+        struct todo_data *ret = malloc(sizeof(struct state_data));
+
+        if (ret == NULL) {
+                return NULL;
+        }
+
+        ret->id = id;
+        ret->priority = priority;
+        ret->state = state;
+        ret->subject = subject;
+        ret->description = description;
+
+        return ret;
+}
+
+struct state_data *create_custom_state_data(bool active, const char *string)
+{
+        struct state_data *ret = malloc(sizeof(struct state_data));
+
+        if (ret == NULL) {
+                return NULL;
+        }
+
+        ret->active = active;
+        ret->custom_state = true;
+        ret->string = string;
+
+        return ret;
+}
+
+struct state_data *create_defined_state_data(bool active,
+                                             enum state_value value)
+{
+        struct state_data *ret = malloc(sizeof(struct state_data));
+
+        if (ret == NULL) {
+                return NULL;
+        }
+
+        ret->active = active;
+        ret->custom_state = false;
+        ret->value = value;
+
+        return ret;
+}
+
+void destroy_state_data(struct state_data *state)
+{
+        free(state);
+}
---
+++ src/data.h
@@ -0,0 +1,43 @@
+// Copyright 2018 Chris Frank
+// Licensed under BSD-3-Clause
+// Refer to the license.txt file included in the root of the project
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+enum state_value {
+        OPEN,
+        IN_PROGRESS,
+        RESOLVED,
+        CLOSED,
+        RE_OPENED,
+};
+
+struct state_data {
+        bool active;
+        bool custom_state;
+        union {
+                enum state_value value;
+                const char *string;
+        };
+};
+
+struct todo_data {
+        uint64_t id;
+        uint64_t priority;
+        struct state_data *state;
+        const char *subject;
+        const char *description;
+};
+
+struct todo_data *create_todo_data(uint64_t id, uint64_t priority,
+                                   const struct state_data *state,
+                                   const char *subject,
+                                   const char *description);
+struct state_data *create_custom_state_data(const char *string);
+struct state_data *create_defined_state_data(enum state_value value);
+
+void destroy_todo_data(struct todo_data *todo);
+void destroy_state_data(struct state_data *state);
